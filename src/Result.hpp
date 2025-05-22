34db71454c8fe14f4857d161de4a00a7d1c1a14d
Bartosz <bartoszbachul1@gmail.com>
2025-05-22 19:13:55 +0200

    Refactor Result class

--- src/Result.hpp
+++ src/Result.hpp
@@ -1,55 +1,125 @@
 #pragma once
 
-#include <string>
+#include <stdexcept>
 
-namespace compressor {
+template <typename T, typename E>
+class Result
+{
+public:
+    Result() = delete;
 
-    template <typename T, typename E = std::string>
-    class Result
+    static Result Ok(const T& value)
     {
+        return Result{true, value, E{}};
+    }
+
+    static Result Err(const E& error)
+    {
+        return Result{false, T{}, error};
+    }
+
+    bool IsOk() const
+    {
+        return m_Success;
+    }
+
+    bool IsErr() const
+    {
+        return !m_Success;
+    }
+
+    T& Value() &
+    {
+        if (!m_Success)
+            throw std::runtime_error{"Attempted to access value in Err result"};
+        return m_Value;
+    }
+
+    const T& Value() const&
+    {
+        if (!m_Success)
+            throw std::runtime_error{"Attempted to access value in Err result"};
+        return m_Value;
+    }
+
+    E& Error()
+    {
+        if (m_Success)
+            throw std::runtime_error{"Attempted to access error in an Ok Result"};
+        return m_Error;
+    }
+
+    const E& Error() const
+    {
+        if (m_Success)
+            throw std::runtime_error{"Attempted to access error in an Ok Result"};
+        return m_Error;
+    }
+
+    explicit operator bool() const
+    {
+        return m_Success;
+    }
+
+private:
+    Result(bool success, const T& value, const E& error)
+        : m_Success{success}, m_Value{value}, m_Error{error}
+    {
+    }
+
+private:
+    bool m_Success;
+    T m_Value;
+    E m_Error;
+};
+
+template <typename E>
+class Result<void, E>
+{
     public:
-        Result() = delete;
+    Result() = delete;
 
-        static Result<T, E> Ok(T&& value)
-        {
-            return Result<T, E>{true, value, E{}};
-        }
+    static Result Ok()
+    {
+        return Result{true, E{}};
+    }
 
-        static Result<T, E> Error(E&& error)
-        {
-            return Result<T, E>{false, T{}, error};
-        }
+    static Result Err(const E& error)
+    {
+        return Result{false, error};
+    }
 
-        bool IsOk() const
-        {
-            return m_Success;
-        }
+    bool IsErr() const
+    {
+        return !m_Success;
+    }
 
-        bool IsErr() const
-        {
-            return !m_Success;
-        }
+    E& Error()
+    {
+        if (m_Success)
+            throw std::runtime_error{"Attempted to access error in an Ok Result"};
+        return m_Error;
+    }
 
-        T&& Value() const
-        {
-            return m_Value;
-        }
+    const E& Error() const
+    {
+        if (m_Success)
+            throw std::runtime_error{"Attempted to access error in an Ok Result"};
+        return m_Error;
+    }
 
-        explicit operator bool()()
-        {
-            return IsOk();
-        }
+    explicit operator bool() const
+    {
+        return m_Success;
+    }
 
-    private:
-        Result(bool success, T&& value, E&& error)
-            : m_Success{success}, m_Value{value}, m_Error{error}
-        {
-        }
+private:
+    Result(bool success, const E& error)
+        : m_Success{success}, m_Error{error}
+    {
+    }
 
-    private:
-        bool m_Success;
-        T m_Value;
-        E m_Error;
-    };
-
-}
+private:
+    bool m_Success;
+    E m_Error;
+};
